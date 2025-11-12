a68042852119f6f86b1365006571a69a9abe8cb5
Aayush Vyas <aayush25vyas@gmail.com>
2025-11-12 17:28:05 +0530

    User schema and model was implemented

--- backend/src/controller/auth/auth.controller.js
+++ backend/src/controller/auth/auth.controller.js
@@ -1,13 +1,24 @@
-
 const authControllers = {
- signup:(request, response) => {
+  signup: (request, response) => {
+    const { fullName, username, password, confirmPassword, gender, email } =
+      request.body;
+    
+      if(!fullName || !username || !password || !confirmPassword || !gender || !email) {
+        return response.status(400).json({success:false, message:"All fields are required"});
+      }
 
- },
- login:(request,response) => {
+    
 
- },
- logout:(request, response) => {
- }
-}
+    try {
+    } catch (error) {
+      console.error(`Error While Signup User: ${error?.message}`);
+      response
+        .status(500)
+        .json({ success: false, message: "Internal Server Error" });
+    }
+  },
+  login: (request, response) => {},
+  logout: (request, response) => {},
+};
 
-export default authControllers; 
+export default authControllers;
