877bf27496420a3803687b4a2c0cf3a5faa2c6fe
Aayush Vyas <aayush25vyas@gmail.com>
2025-11-19 17:15:47 +0530

    Working on logout functionality

--- frontend/src/components/ButtonComponents/LogoutButtonComponent.jsx
+++ frontend/src/components/ButtonComponents/LogoutButtonComponent.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { BiLogOut } from 'react-icons/bi'
-import {ButtonComponent} from "../index"
-import { useLogout } from '../../hooks'
+import React from "react";
+import { BiLogOut } from "react-icons/bi";
+import { ButtonComponent, IconComponent, SpinnerLoader } from "../index";
+import { useLogout } from "../../hooks";
 
 function LogoutButtonComponent() {
-    const {loading , logout} = useLogout();
+  const { loading, logout } = useLogout();
 
   return (
-     <div className="mt-auto">
-        <ButtonComponent btnIcon={ !loading && BiLogOut } btnIconSize={5} onClickHandler={logout} />
-      </div>
-  )
+    <div className="mt-auto">
+      <ButtonComponent
+        btnIconSize={5}
+        onClickHandler={logout}
+        btnText={!loading ? <IconComponent icon={BiLogOut} iconSize={5} /> : <SpinnerLoader />}
+      />
+    </div>
+  );
 }
 
-export default LogoutButtonComponent
+export default LogoutButtonComponent;
